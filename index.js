b1949ba99c1fa1540abc6406f51d241828de759e
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-28 12:00:49 -0700

    working on finding a person

--- index.js
+++ index.js
@@ -64,17 +64,69 @@ class BinaryTreeNode {
   }
 }
 
-const B = new BinaryTreeNode('B');
-const A = new BinaryTreeNode('A');
-const C = new BinaryTreeNode('C');
-const D = new BinaryTreeNode('D');
+const B = new BinaryTreeNode("B");
+const A = new BinaryTreeNode("A");
+const C = new BinaryTreeNode("C");
+const D = new BinaryTreeNode("D");
 
 // B will be the root of the tree:
-B.add(A);
-console.log(B);
-B.add(D);
-console.log(B);
-B.add(C);
-console.log(B);
+// B.add(A);
+// console.log(B);
+// B.add(D);
+// console.log(B);
+// B.add(C);
+// console.log(B);
 
-module.exports = { LinkedList, BinaryTreeNode };
+class PersonTreeNode {
+  constructor(person) {
+    this.value = person.name;
+    this.person = person;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(node) {
+    if (node.value > this.value) {
+      if (!this.right) {
+        this.right = node;
+      } else {
+        this.right.add(node);
+      }
+    }
+    if (node.value < this.value) {
+      if (!this.left) {
+        this.left = node;
+      } else {
+        this.left.add(node);
+      }
+    }
+  }
+
+  findPerson(name) {
+    if (name === this.value) {
+      return this.person;
+    } else if (name === this.right.value) {
+      return this.right.person;
+    } else if (name === this.left.value) {
+      return this.left.person;
+    } else if (name > this.value) {
+        this.right.findPerson(name);
+      } else {
+        this.left.findPerson(name);
+      }
+  }
+
+
+// "root" is the node at the root of the tree (the tree)
+// let person = root.find("Nelson");
+// // person looks like
+// // {
+// //   name: 'Nelson',
+// //   phone: '555-1212',
+// //   address: '123 Main St'
+// // }
+
+// let person2 = root.find("Foo");
+// person2 is null
+
+module.exports = { LinkedList, BinaryTreeNode, PersonTreeNode };
