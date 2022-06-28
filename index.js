8a9ba739a5fc44f5d558b3c22eb45a9c9318b744
Riley Hoffman <rileyjhoff@gmail.com>
2022-06-28 11:37:29 -0700

    write binary-node-add

--- index.js
+++ index.js
@@ -32,11 +32,49 @@ class LinkedList {
   }
 }
 
-const list = new LinkedList("A");
-console.log(list.head);
-list.add("B");
-console.log(list.getList());
-list.add("C");
-console.log(list.getList());
+// const list = new LinkedList('A');
+// console.log(list.head);
+// list.add('B');
+// console.log(list.getList());
+// list.add('C');
+// console.log(list.getList());
 
-module.exports = { LinkedList };
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(node) {
+    if (node.value > this.value) {
+      if (!this.right) {
+        this.right = node;
+      } else {
+        this.right.add(node);
+      }
+    }
+    if (node.value < this.value) {
+      if (!this.left) {
+        this.left = node;
+      } else {
+        this.left.add(node);
+      }
+    }
+  }
+}
+
+const B = new BinaryTreeNode('B');
+const A = new BinaryTreeNode('A');
+const C = new BinaryTreeNode('C');
+const D = new BinaryTreeNode('D');
+
+// B will be the root of the tree:
+B.add(A);
+console.log(B);
+B.add(D);
+console.log(B);
+B.add(C);
+console.log(B);
+
+module.exports = { LinkedList, BinaryTreeNode };
